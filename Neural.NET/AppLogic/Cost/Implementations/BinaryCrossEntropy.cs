46f9bbd65d52fbe421d31c4acfa9b937289e3d66
kordellgifford <kordellgifford@conservice.com>
2022-12-02 16:47:18 -0700

    Add costs

---
+++ Neural.NET/AppLogic/Cost/Implementations/BinaryCrossEntropy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Numpy;
+
+namespace Neural.NET.AppLogic.Cost.Implementations
+{
+    internal class BinaryCrossEntropy
+    {
+        public NDarray ComputeCost(NDarray AL, NDarray Y)
+        {
+            var m = Y.shape[1];
+
+            var cost = -(1 / m) * np.sum(np.multiply(Y, np.log(AL)) + np.multiply((1 - Y), np.log(1 - AL)));
+            cost = np.squeeze(cost);
+
+            return cost;
+        }
+    }
+}
